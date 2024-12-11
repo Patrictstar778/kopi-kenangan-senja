d13a8b380eb45d285eb585192d64d8d47f97ebd1
Patrictstar778 <ekasatryo071005@gmail.com>
2024-12-11 11:27:38 +0700

    Add files via upload

---
+++ kopi-kenangan-senja/js/script.js
@@ -0,0 +1,15 @@
+//Toggle clss active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klick diluar si debar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
